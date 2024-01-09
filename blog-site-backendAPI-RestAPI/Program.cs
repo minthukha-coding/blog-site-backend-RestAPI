1ed84c48757a87ac678fac9d0fe77f944e752ce4
minthukha-coding <minthukha1239788@gmail.com>
2024-01-09 21:14:54 +0630

    Complete Blog Site API

--- blog-site-backendAPI-RestAPI/Program.cs
+++ blog-site-backendAPI-RestAPI/Program.cs
@@ -17,6 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseAuthorization();
 
